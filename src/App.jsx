b23b2e561f1cdad3d845f815c6f577f8df1f7baa
WayneLinnn <128369499+WayneLinnn@users.noreply.github.com>
2026-08-17 00:19:10 +1000

    App.jsx: add Lenis setup

--- src/App.jsx
+++ src/App.jsx
@@ -7,5 +7,25 @@ import Scene from "./Scene.jsx";
 gsap.registerPlugin(ScrollTrigger);
 
 export default function App() {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    });
+
+    lenis.on("scroll", ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return null;
 }
