39835328e4c7ab362b7316fc9adbef5c7cce6f99
WayneLinnn <128369499+WayneLinnn@users.noreply.github.com>
2026-08-17 00:19:01 +1000

    App.jsx: add imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Lenis from "lenis";
+import Scene from "./Scene.jsx";
+
+gsap.registerPlugin(ScrollTrigger);
+
 export default function App() {
   return null;
 }
